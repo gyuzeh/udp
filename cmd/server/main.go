4a680feb962d96e07ca4e8f61b4a4d0726b8af42
Daniel Cibrão <daniel.cibrao@farfetch.com>
2021-06-05 16:04:23 +0100

    feat: updserver implementation logic

---
+++ cmd/server/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/gyuzeh/udp/pkg/server"
+)
+
+func main() {
+	arguments := os.Args
+
+	if len(arguments) == 1 {
+		fmt.Println("Please provide a port number!")
+		return
+	}
+
+	port := arguments[1]
+
+	udpServer, _ := server.StartUDPServer(port, "udp4")
+	udpServer.Listen()
+}
